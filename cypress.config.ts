a938244ee58493f8c50554fbc3f930ce972e726e
sowmia.b <765727@cognizant.com>
2023-12-14 20:28:50 +0530

    fix(test):  downgraded typescript version

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,10 @@
 export default {
   projectId: '3tqx64',
   e2e: {
+    specPattern: 'cypress/e2e/**/*.spec.ts',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  
 };
